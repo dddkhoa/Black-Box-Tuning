1f71593285b6e2720493e390daf923044ba284c4
dddkhoa <dddkhoa.vn@gmail.com>
2023-04-16 14:02:30 +0700

    feat: Added bayesian opt

---
+++ run_bayes.sh
@@ -0,0 +1,12 @@
+python bayesian_opt.py \
+  --task_name "sst2" \
+  --n_prompt_tokens 50 \
+  --intrinsic_dim 500 \
+  --k_shot 16 \
+  --device "cuda:0" \
+  --seed 42 \
+  --loss_type "ce" \
+  --cat_or_add "add" \
+  --budget 500 \
+  --print_every 50 \
+  --eval_every 100
