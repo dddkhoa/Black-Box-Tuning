712485ef4493dd1a323df53596f324dd6d4ebb5a
dddkhoa.vn <dddkhoa.vn@gmail.com>
2023-05-08 18:29:38 +0000

    chore: edit run config

--- run_bayes.sh
+++ run_bayes.sh
@@ -1,12 +1,12 @@
 python bayesian_opt.py \
   --task_name "sst2" \
   --n_prompt_tokens 50 \
-  --intrinsic_dim 500 \
+  --intrinsic_dim 20 \
   --k_shot 16 \
   --device "cuda:0" \
   --seed 42 \
   --loss_type "ce" \
   --cat_or_add "add" \
-  --budget 200 \
-  --print_every 50 \
-  --eval_every 100
+  --budget 8000 \
+  --print_every 10 \
+  --eval_every 10
